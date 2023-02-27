83a64d66e06393bdf2137de6ab0f2472682dfba4
Dulguuuuuuuun <Dulguunpalam23@gmail.com>
2023-02-28 01:06:08 +0800

    messanger jijghn heseg

--- frontend/my-app/src/pages/home.jsx
+++ frontend/my-app/src/pages/home.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "../App.css";
 import SideBar from "../components/sidebar";
+import Post from "../components/Post";
+import WriteMessage from "../components/WriteMessage";
+
 function home() {
   return (
     <>
       <div className="homeContainer">
         <SideBar />
+        <WriteMessage />
+        {/* <Post /> */}
       </div>
     </>
   );
