7bd1e5e5cc3580287e89b2ebeba6cf66124c45f8
Dulguuuuuuuun <Dulguunpalam23@gmail.com>
2023-03-01 23:50:48 +0800

    s

--- frontend/my-app/src/pages/home.jsx
+++ frontend/my-app/src/pages/home.jsx
@@ -5,6 +5,8 @@ import SideBar from "../components/sidebar";
 import WriteMessage from "../components/WriteMessage";
 import Search from "../components/Search";
 
+import NewMessage from "../components/NewMessage";
+import Popup from "../components/Popup";
 function home() {
   return (
     <>
@@ -14,6 +16,9 @@ function home() {
         {/* <WriteMessage /> */}
         {/* <Post /> */}
         <Search />
+        {/* <SideBar />
+        <NewMessage /> */}
+        {/* <Popup /> */}
       </div>
     </>
   );
