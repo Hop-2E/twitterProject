c695c19eacf1ba9b44cb4e677bd1ff6c7ec8d307
Dulguuuuuuuun <Dulguunpalam23@gmail.com>
2023-03-02 16:28:16 +0800

    searchte heseg yrn duussan

--- frontend/my-app/src/pages/home.jsx
+++ frontend/my-app/src/pages/home.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import "../App.css";
 import SideBar from "../components/sidebar";
-import Post from "../components/Post";
+// import Post from "../components/Post";
 import WriteMessage from "../components/WriteMessage";
+import Search from "../components/Search";
 
 function home() {
   return (
     <>
       <div className="homeContainer">
         <SideBar />
-        <WriteMessage />
+        {/* <WriteMessage /> */}
         {/* <Post /> */}
+        <Search />
       </div>
     </>
   );
