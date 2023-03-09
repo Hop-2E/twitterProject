38a470a87128e601548a94fbb8622227138990ec
bayr_aaaaa <bayra_aaaaa@bayr-aaaaas-MacBook-Pro.local>
2023-03-09 16:26:16 +0800

    ahah

--- frontend/my-app/src/pages/home.jsx
+++ frontend/my-app/src/pages/home.jsx
@@ -1,38 +1,87 @@
 import React from "react";
 import "../App.css";
 import SideBar from "../components/sidebar";
-// import Post from "../components/Post";
-// import WriteMessage from "../components/WriteMessage";
-
-// import NewMessage from "../components/NewMessage";
-
-import Popup from "../components/Popup";
-import Header from "../components/Header";
-import Login from "../components/Login";
-import FollowBox from "../components/FollowBox";
-
-// import Popup from "../components/Popup";
-import FooterSignupLogin from "../components/footerSignupLogin";
-
+import { CiImageOn } from "react-icons/ci";
+import { HiOutlineGif } from "react-icons/hi2";
+import { BiPoll } from "react-icons/bi";
+import { BsEmojiSmile } from "react-icons/bs";
+import { TbCalendarTime } from "react-icons/tb";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import RightSideBarHome from "../components/rigthSideBarHome";
 function home() {
   return (
     <>
       <div className="homeContainer">
         <SideBar />
-        {/* <WriteMessage /> */}
-        {/* <Header /> */}
-        {/* <Search /> */}
-
-        {/* <SideBar />
-        <NewMessage /> */}
-        {/* <Popup /> */}
-        <Login />
-        {/* <FollowBox /> */}
-
-        {/* <SideBar /> */}
-        {/* <NewMessage />
-        <Popup /> */}
-        <FooterSignupLogin />
+        <div className="mainContentHome">
+          <div className="homeContentHeader">
+            <div className="pageNameDiv">
+              <p className="pageNameHomeContainer">Home</p>
+            </div>
+            <div className="fypHome">
+              <div className="fypHomeTextsDiv">
+                <span className="fypHomeTexts">For you</span>
+              </div>
+              <div className="fypHomeTextsDiv">
+                <span className="fypHomeTexts">Following</span>
+              </div>
+            </div>
+          </div>
+          <div className="homeTweet">
+            <div className="homeTweetHeader">
+              <div className="homeTweetHeaderHeader">
+                <div className="homeTweetHeaderHeaderPic"></div>
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  className="homeTweetHeaderHeaderInput"
+                  placeholder="What's happening?"
+                />
+              </div>
+              <div className="homeTweetBottom">
+                <div className="homeTweetBottomIconsDiv">
+                  <div className="iconDiv">
+                    <CiImageOn className="iconHomeTweetBottom" />
+                  </div>
+                  <div className="iconDiv">
+                    {" "}
+                    <HiOutlineGif className="iconHomeTweetBottom" />
+                  </div>
+                  <div className="iconDiv">
+                    <BiPoll className="iconHomeTweetBottom" />
+                  </div>
+                  <div className="iconDiv">
+                    {" "}
+                    <BsEmojiSmile
+                      className="iconHomeTweetBottom"
+                      style={{ fontSize: "17px", marginBottom: "2px" }}
+                    />
+                  </div>
+                  <div className="iconDiv">
+                    {" "}
+                    <TbCalendarTime
+                      className="iconHomeTweetBottom"
+                      style={{ fontSize: "19px", marginBottom: "2px" }}
+                    />
+                  </div>
+                  <div className="iconDiv">
+                    <HiOutlineLocationMarker
+                      className="iconHomeTweetBottom"
+                      style={{ fontSize: "19px", marginBottom: "1px" }}
+                    />
+                  </div>
+                </div>
+                <div className="homeTweetButtonDiv">
+                  <button className="homeTweetButton">Tweet</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="homeRightSiderBar">
+          <RightSideBarHome />
+        </div>
       </div>
     </>
   );
